b3701d5d1507a12f2ff9931c82d7286ed2a58fd2
Emma <55496037+emmasmithdev@users.noreply.github.com>
2020-03-20 13:25:42 +0000

    set up mvc home controller and index view

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Emma's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Indian Scene", Cuisine = CuisineType.French},
+                new Restaurant { Id = 3, Name = "Genova", Cuisine = CuisineType.Italian},
+            };
+        }
+
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
