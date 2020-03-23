ae62e14a90851f937868bed1e2f8bc87de9ea709
Emma <55496037+emmasmithdev@users.noreply.github.com>
2020-03-23 17:16:45 +0000

    sql server db

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -10,8 +10,11 @@ namespace OdeToFood.Data.Models
     public class Restaurant
     {
         public int Id { get; set; }
+
         [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
         public CuisineType Cuisine { get; set; }
 
     }
