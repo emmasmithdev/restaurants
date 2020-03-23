2c45b1cc4e60ec1f4283da2ffbb536868ff99d2a
Emma <55496037+emmasmithdev@users.noreply.github.com>
2020-03-23 09:51:31 +0000

    get and post requests

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -1,4 +1,5 @@
-﻿using OdeToFood.Data.Services;
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,21 +17,41 @@ namespace OdeToFood.Web.Controllers
             this.db = db;
         }
 
-        // GET: Restaurants
+        [HttpGet]
         public ActionResult Index()
         {
             var model = db.GetAll();
             return View(model);
         }
 
+        [HttpGet]
         public ActionResult Details(int id)
         {
             var model = db.Get(id);
-            if(model == null)
+            if (model == null)
             {
-                return View("NotFound"); 
+                return View("NotFound");
             }
             return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+
+            if(ModelState.IsValid)
+            {
+                db.Add(restaurant);
+                return View();
+            }
+            return View();
         }
     }
 }
